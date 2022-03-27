dfc717b02830d84597fa05ca3b2292295bbe11c7
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-03-27 14:25:45 -0300

    ..

--- happy_numbers.py
+++ happy_numbers.py
@@ -1,7 +1,7 @@
-'''
+"""
 Ejercicio 1 * Happy Numbers
 Hacer un programa que imprima los X primeros “happy numbers”.
-'''
+"""
 
 def pdi_function(number, power, base: int = 10):
     """Perfect digital invariant function."""
@@ -12,7 +12,15 @@ def pdi_function(number, power, base: int = 10):
     return total
 
 def is_happy(number: int, goal_value: int = 1, power: int=2) -> bool:
-    """Determine if the specified number is a happy number."""
+    """Determina si el número especificado es un 'happy number'.
+    
+    Args:
+        number: número del que se desea saber si es 'happy number'.
+        goal_value: valor final al que debe llegar la operación para que se considere 'happy number'.
+        power: exponente de la potencia que se aplica en 'pdi_function()'.
+    Returns:
+        Devuelve un valor booleano que indica si el número es un 'happy number'.
+    """
     seen_numbers = set()
     while number != goal_value and number not in seen_numbers:
         seen_numbers.add(number)
@@ -20,6 +28,13 @@ def is_happy(number: int, goal_value: int = 1, power: int=2) -> bool:
     return number == goal_value
 
 def happy_numbers(n, goal_value: int=1, power: int=2):
+    """ Imprime los n primeros 'happy numbers'.
+    
+    Args:
+        n: cantidad de valores que queremos que se impriman.
+        goal_value (Opcional): valor final al que debe llegar la operación para que se considere 'happy number'.
+        power (Opcional): exponente de la potencia que se aplica en 'pdi_function()'.
+    """
     happy_list = []
     number = 1
     while len(happy_list) < n:
@@ -29,4 +44,9 @@ def happy_numbers(n, goal_value: int=1, power: int=2):
         number += 1
     print(happy_list)
 
+
+""" 
+Ejercicio 1. Happy numbers.
+Variación A. 
+ """
 
